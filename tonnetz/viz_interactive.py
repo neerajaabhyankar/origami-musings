2b147f389e43bfee3375e13299813a358a970020
Neeraja Abhyankar <neeraja.abhyankar@gmail.com>
2025-04-10 21:22:36 -0700

    backup

--- tonnetz/viz_interactive.py
+++ tonnetz/viz_interactive.py
@@ -6,12 +6,20 @@ swarvals = [
     ratio_to_swarval(tn.node_frequencies[ii]) for ii in range(len(tn.node_names))
 ]
 
-# shortcut: hardcoding
-integer_swarvals = [2, 4, 7, 9]
-parallax_x = 0.7
-parallax_y = 0.2
-
 achal_nodes = [(0, 0), (1, 0)]
+
+# for an arbitrary raag
+raag_nodecolor = {
+    "custom": "#f08b65",
+}
+raag_gt_nodes = {
+    "custom": [],
+}
+nnotes = input("Enter the number of notes in your Raag:\n")
+for ii in range(int(nnotes)):
+    print(f"Enter the prime factorization of note {ii+1} in the format `q3, q5`:")
+    powers = input().split(", ")
+    raag_gt_nodes["custom"].append(tuple(map(int, powers)))
 
 
 """ interactive plot """
@@ -34,9 +42,13 @@ fig.add_trace(go.Scatter3d(
 
 # raag-specific nodes
 for raag, gt_nodes in raag_gt_nodes.items():
-    if raag == "Yaman":
-        continue
-    gt_node_indices = [tn.node_coordinates.index(coord) for coord in gt_nodes if coord not in achal_nodes]
+    # for the Bhoop-Deshkar viz
+    # if raag == "Yaman":
+    #     continue
+    # gt_node_indices = [tn.node_coordinates.index(coord) for coord in gt_nodes if coord not in achal_nodes]
+    
+    # for an arbitrary raag
+    gt_node_indices = [tn.node_coordinates.index(coord) for coord in gt_nodes]
 
     fig.add_trace(go.Scatter3d(
         x=np.array(tn.coords3d[0])[gt_node_indices],
@@ -54,42 +66,42 @@ for raag, gt_nodes in raag_gt_nodes.items():
         showlegend=False,
     ))
 
-# syntonic comma
-idx_D1 = 18
-idx_D2 = 37
-ratios = ["5/3", "27/16"]
-fig.add_trace(go.Scatter3d(
-    x=[tn.coords3d[0][idx_D1], tn.coords3d[0][idx_D2]],
-    y=[tn.coords3d[1][idx_D1], tn.coords3d[1][idx_D2]],
-    z=[swarvals[idx_D1] + ANNOTATION_OFFSET, swarvals[idx_D2] + ANNOTATION_OFFSET],
-    mode="text",
-    text=ratios,
-    textposition="middle center",
-    textfont=dict(family="Overpass", size=1.5*DOT_LABEL_SIZE, color=ANNOTATION_GREEN),
-    showlegend=False,
-))
+# # syntonic comma
+# idx_D1 = 18
+# idx_D2 = 37
+# ratios = ["5/3", "27/16"]
+# fig.add_trace(go.Scatter3d(
+#     x=[tn.coords3d[0][idx_D1], tn.coords3d[0][idx_D2]],
+#     y=[tn.coords3d[1][idx_D1], tn.coords3d[1][idx_D2]],
+#     z=[swarvals[idx_D1] + ANNOTATION_OFFSET, swarvals[idx_D2] + ANNOTATION_OFFSET],
+#     mode="text",
+#     text=ratios,
+#     textposition="middle center",
+#     textfont=dict(family="Overpass", size=1.5*DOT_LABEL_SIZE, color=ANNOTATION_GREEN),
+#     showlegend=False,
+# ))
 
-fig.add_trace(go.Scatter3d(
-    x=[tn.coords3d[0][idx_D1], tn.coords3d[0][idx_D2]],
-    y=[tn.coords3d[1][idx_D1], tn.coords3d[1][idx_D2]],
-    z=[swarvals[idx_D1], swarvals[idx_D2]],
-    mode="lines",
-    line=dict(color=ANNOTATION_GREEN, width=5),
-    showlegend=False,
-))
-fig.add_trace(go.Scatter3d(
-    x=[(tn.coords3d[0][idx_D1] + tn.coords3d[0][idx_D2]) / 2],
-    y=[(tn.coords3d[1][idx_D1] + tn.coords3d[1][idx_D2]) / 2],
-    z=[(swarvals[idx_D1] + swarvals[idx_D2])/2 + ANNOTATION_OFFSET/2],
-    mode="text",
-    text=["syntonic comma"],
-    textposition="middle center",
-    textfont=dict(family="Overpass", size=1.5*DOT_LABEL_SIZE, color=ANNOTATION_GREEN),
-    showlegend=False,
-))
+# fig.add_trace(go.Scatter3d(
+#     x=[tn.coords3d[0][idx_D1], tn.coords3d[0][idx_D2]],
+#     y=[tn.coords3d[1][idx_D1], tn.coords3d[1][idx_D2]],
+#     z=[swarvals[idx_D1], swarvals[idx_D2]],
+#     mode="lines",
+#     line=dict(color=ANNOTATION_GREEN, width=5),
+#     showlegend=False,
+# ))
+# fig.add_trace(go.Scatter3d(
+#     x=[(tn.coords3d[0][idx_D1] + tn.coords3d[0][idx_D2]) / 2],
+#     y=[(tn.coords3d[1][idx_D1] + tn.coords3d[1][idx_D2]) / 2],
+#     z=[(swarvals[idx_D1] + swarvals[idx_D2])/2 + ANNOTATION_OFFSET/2],
+#     mode="text",
+#     text=["syntonic comma"],
+#     textposition="middle center",
+#     textfont=dict(family="Overpass", size=1.5*DOT_LABEL_SIZE, color=ANNOTATION_GREEN),
+#     showlegend=False,
+# ))
 
-# send the last two traces to the back
-fig.data = (*fig.data[-2:], *fig.data[:-2])
+# # send the last two traces to the back
+# fig.data = (*fig.data[-2:], *fig.data[:-2])
 
 # custom legend
 
